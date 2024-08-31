9c51c8b10406818e493f3cdc284361d7a9f32cd5
K1ngHandy <bolds-slicks0g@icloud.com>
2024-08-31 16:51:19 -0400

    Initial Commit

---
+++ MeetMapKit/MeetMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMapKitApp.swift
+//  MeetMapKit
+//
+//  Created by Steve Handy on 2024.08.31.
+//
+
+import SwiftUI
+
+@main
+struct MeetMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeetMapKitUITests/MeetMapKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MeetMapKitUITestsLaunchTests.swift
+//  MeetMapKitUITests
+//
+//  Created by Steve Handy on 2024.08.31.
+//
+
+import XCTest
+
+final class MeetMapKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
